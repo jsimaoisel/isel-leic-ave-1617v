4bbe3136a82331b1b4bbf43af47cffbf2c97c3d8
José Simão <jsimao@cc.isel.ipl.pt>
2017-04-27 11:45:08 +0100

    aula 15

---
+++ aula15/Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class TargetMethods
+    {
+        public delegate void Action(int x);
+        public delegate void OtherAction(int x);
+
+        public static void DoAction(Action a, int i) { a(i); }
+        public static void SomeAction(int i) { Console.WriteLine(i); }
+
+        public static void Main(String[] args)
+        {
+            // 1.
+            OtherAction o = SomeAction;
+            DoAction(SomeAction, 1024);
+            DoAction(o, 10);
+
+            // 2.
+            Action a0 = delegate (int x) { Console.Beep(1500, x); };
+            Action a1 = SomeAction;
+            Action a2 = (Action)Delegate.Combine(a1, a0, a0);
+            a2(750);
+
+            a2 -= new Action(SomeAction);
+            a2(750);
+        }
+    }
+}
---
+++ aula15/Hierarchy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+namespace Hierarchy
+{
+    interface I
+    {
+        void M1();
+    }
+    class A : I
+    {
+        public virtual void M1() { Console.WriteLine("A::M1"); }
+        public virtual void M2() { Console.WriteLine("A::M2"); }
+    }
+    class B : A
+    {
+        public override void M1() { Console.WriteLine("B::M1"); }
+        public override void M2() { Console.WriteLine("B::M2"); }
+    }
+    class C : B
+    {
+        public new void M1() { Console.WriteLine("C::M1"); }
+        public override void M2() { Console.WriteLine("C::M2"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            I i = new C();
+            i.M1();
+
+            A a = ((A)i);
+            a.M2();
+            a.M1();
+        }
+    }
+}
