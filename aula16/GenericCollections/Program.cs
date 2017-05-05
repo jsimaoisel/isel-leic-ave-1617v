9e5f5a516c4dd874719429b5b7e855a352f5bcf7
José Simão <jsimao@cc.isel.ipl.pt>
2017-05-05 22:02:31 +0100

    aula 16

---
+++ aula16/GenericCollections/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrayList = new List<string>();
+
+            LinkedList<SortedSet<string>> linkedList = new LinkedList<SortedSet<string>>();
+
+            Dictionary<int, SortedSet<string>> ht = new Dictionary<int, SortedSet<string>>();
+
+            foreach(string v in arrayList)
+            {
+                // print v
+            }
+
+            IEnumerable<int> seq = MakeSequence(10);
+            foreach (int i in seq)
+            {
+                Console.WriteLine(i);
+            }
+
+            IEnumerator<int> it = MakeSequence(10).GetEnumerator();
+            while (it.MoveNext())
+            {
+                int i = it.Current;
+                Console.WriteLine(i);
+            }
+            
+        }
+
+        private static IEnumerable<int> MakeSequence(int v)
+        {
+            return new NIEnumerable(v);
+        }
+    }
+
+    class NIEnumerable : IEnumerable<int>
+    {
+        int limit;
+        public NIEnumerable(int limit)
+        {
+            this.limit = limit;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new NIEnumerator(limit);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new NIEnumerator(limit);
+        }
+    }
+
+    class NIEnumerator : IEnumerator<int>
+    {
+        int limit;
+        int current = -1;
+        public NIEnumerator(int limit)
+        {
+            this.limit = limit;
+        }
+        public int Current
+        {
+            get
+            {
+                return current;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return current;
+            }
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public bool MoveNext()
+        {
+            current++;
+            return current <= limit;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ aula16/GenericType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericType
+{
+    class AGenericType<T, U> //where U : class where T : IComparable<U>
+    {
+        public T P { get; set; }
+
+        public W M<W>(T t, U u) where W : new()
+        {
+            /*...*/
+            return new W();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type open = typeof(AGenericType<,>);
+            Type closed = typeof(AGenericType<string, int>);
+        }
+    }
+}
