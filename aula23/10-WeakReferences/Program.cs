ddb7e231326c17a96be3d5b857796aed855554d1
José Simão <jsimao@cc.isel.ipl.pt>
2017-06-09 14:25:10 +0100

    aula 23: Finalizadores e Padrão Dispose; Referências fracas

---
+++ aula23/10-WeakReferences/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _10_WeakReferences
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a strong reference to a new object.
+            Student s = new Student();
+            // Create a strong reference to a short WeakReference object.
+            // The WeakReference object tracks the Object's lifetime.
+            WeakReference<Student> wr = new WeakReference<Student>(s);
+            s = null; // Remove the strong reference to the object.
+            if (wr.TryGetTarget(out s))
+            {
+                // A garbage collection occurred and Object's was reclaimed
+            }
+            else {
+                // A garbage collection did not occur and I can successfully access
+                // the object using o.
+            }
+        }
+    }
+}
---
+++ aula23/Dispose/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dispose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream("aaa", FileMode.Open))
+            {
+                fs.Write(new byte[] { 1, 2, 3 },0,3);
+            }
+        }
+    }
+}
