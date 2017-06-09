f86f361c21b66374e3d15df6b7813b6f5c35c1bf
José Simão <jsimao@cc.isel.ipl.pt>
2017-06-09 14:19:41 +0100

    aula 22 - intro GC

---
+++ aula22/aula22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace aula22
+{
+    class X {
+        int[] a1 = new int[512];
+        int[] a2  = new int[512];
+        int[] a3 = new int[512];
+    }
+    class Program
+    {
+        static List<X> list = new List<X>();
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            for (;;)
+            {
+                X p = new X();
+                Thread.Sleep(10);
+                if (++counter % 5 == 0)
+                {
+                    list.Add(p);
+                }
+            }
+        }
+    }
+}
---
+++ aula22/events/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace events
+{
+    class Button
+    {
+        public event Action<String> OnClick;
+        /*
+         * private Action<String> OnClick;
+         * 
+         * public void add_OnClick(Action<String> a) {
+         *      OnClick += a; //  OnClick = (Action<String>) Delgate.Combine(OnClick, a);
+         * }
+         * 
+         * public void remove_OnClick(Action<String> a)
+         *      OnClick -= a;
+         * }
+         */
+        public void m()
+        {
+            if (OnClick != null)
+            {
+                OnClick.Invoke("AVE");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        int x;
+
+
+
+        static void Main(string[] args)
+        {
+            Button btn = new Button();
+            btn.OnClick += x => Console.WriteLine("OnClick");
+        }
+    }
+}
